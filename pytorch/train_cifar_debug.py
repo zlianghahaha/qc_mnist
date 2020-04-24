6d4ed73bd98864f8272a80fabe7632108d3abf74
weiwenjiang <jiang.wwen@gmail.com>
2020-04-23 22:43:54 -0400

    delete dataset

--- pytorch/train_cifar_debug.py
+++ pytorch/train_cifar_debug.py
@@ -32,7 +32,7 @@ print = functools.partial(print, flush=True)
 
 # interest_num = [0,1,2,3,4,5,6,7,8,9]
 interest_num = [1, 2, 7]
-img_size = 32
+img_size = 16
 # number of subprocesses to use for data loading
 num_workers = 0
 # how many samples per batch to load
