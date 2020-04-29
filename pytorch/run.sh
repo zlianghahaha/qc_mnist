cc8aa1bdd10ea2758bad9abb33d91fa4830b24d2
weiwenjiang <jiang.wwen@gmail.com>
2020-04-29 08:15:37 -0400

    executable codes

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -1,7 +1,6 @@
-python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
+CUDA_VISIBLE_DEVICES=$1 python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
 sleep 1
-python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
+CUDA_VISIBLE_DEVICES=$1 python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
 sleep 1
-
-wait()
+wait
 
