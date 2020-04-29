8559744cc07da8b3a92c692c4fa28d15824abe42
weiwenjiang <jiang.wwen@gmail.com>
2020-04-29 08:14:09 -0400

    executable codes

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -1,12 +1,7 @@
 python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
-sleep(1)
+sleep 1
 python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
-sleep(1)
-python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
-sleep(1)
-python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
-sleep(1)
-python exe_mnist.py -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk &
+sleep 1
 
 wait()
 
