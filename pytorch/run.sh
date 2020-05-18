5d2d9272c9711ccbc9162593451e409b2df963ca
weiwenjiang <jiang.wwen@gmail.com>
2020-05-18 10:38:59 -0400

    binarize only first layer

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -66,35 +66,35 @@
 ## 36
 #
 #
-#CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnqt1_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnqt2_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnqt3_36_bin.res 2>&1 &
-#sleep 1
-#
-#CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/q1_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/q2_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/q3_36_bin.res 2>&1 &
-#sleep 1
-#wait
-#
-#CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnnq1_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnnq2_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnnq3_36_bin.res 2>&1 &
-#sleep 1
-#
-#CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/nq1_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/nq2_36_bin.res 2>&1 &
-#sleep 1
-#CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/nq3_36_bin.res 2>&1 &
-#sleep 1
-#wait
+CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnqt1_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnqt2_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -qt -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnqt3_36_bin.res 2>&1 &
+sleep 1
+
+CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/q1_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/q2_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/q3_36_bin.res 2>&1 &
+sleep 1
+wait
+
+CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnnq1_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnnq2_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=0 python exe_mnist.py -bin -wn -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/wnnq3_36_bin.res 2>&1 &
+sleep 1
+
+CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/nq1_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/nq2_36_bin.res 2>&1 &
+sleep 1
+CUDA_VISIBLE_DEVICES=1 python exe_mnist.py -bin -nq -c "3, 6" -s 4 -l 0.1 -e 30 -m "10, 20" -chk > log/nq3_36_bin.res 2>&1 &
+sleep 1
+wait
 #
 #
 ## 38
